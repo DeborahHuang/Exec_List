740d8cce0dc8298bdd9b8160e53c6b4f7a113a5f
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:11:23 +0800

    加入專案檔案。

---
+++ Exec_List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_List
+{//Exec_List 宣告一個 List<Product> 變數, 存放多筆購買商品
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> Product = new List<string>()
+			{
+				"book1",
+			    "book2",
+				"book3",
+				"book4",
+				"book5",
+			};
+		}
+	}
+}
